7e7c8b0e65827b7a50df90741f90552a3cd25991
nbeers22 <nbeers22@gmail.com>
2017-08-24 15:25:00 -0400

    Added contact form styles on contact us page. Minor fix to contact sidebar

--- themes/clearplex/page-contact.php
+++ themes/clearplex/page-contact.php
@@ -2,35 +2,34 @@
 	/*
 	Template Name: Contact
 	*/
-	get_header(); 
-	$bg_img = wp_get_attachment_url(get_post_thumbnail_id( $post->ID ));
+	get_header();
 	get_template_part('template-parts/top-bg');
 ?>
 <div class="sub-head">
 	<div class="sub-img small-10 small-centered">
 		<?php echo wp_get_attachment_image( get_post_thumbnail_id(), 'fp-large' );  ?>
 		<h2 class="blue-heading"><?php echo ( get_post_meta( $post->ID, 'contact_title', true) ? get_post_meta( $post->ID, 'contact_title', true) : get_the_title() ); ?></h2>
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+			
+			<div class="entry-content large-11">
+				<p class="gray-p"><?php echo get_the_content(); ?></p>
+			</div>
+			<?php endwhile; ?>
+
+			<?php else : ?>
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+			<?php endif; // End have_posts() check. ?>
 	</div>
 </div>
 <!-- standard loop -->
 <div class="main-wrap" role="main">
 	<div class="small-10 small-centered">
 	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
-
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-		<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-		<div class="entry-content large-11">
-			<?php the_content(); ?>
-		</div>
-		<?php endwhile; ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; // End have_posts() check. ?>
-
+		<?php echo do_shortcode('[ninja_form id=1]'); ?>
 	</article>
 	<?php get_sidebar('contact'); ?>
 	</div>
